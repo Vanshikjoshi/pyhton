e846162d7f8860c6e8c7d817ec449689d2cbd377
Vanshika <vanshikajoshi072@gmail.com>
2026-09-02 19:46:17 +0530

    learn about the escape sequence, operators and did some questions

---
+++ session2.py
@@ -0,0 +1,25 @@
+#escape sequences
+"""
+print("Hello\nWorld");
+print("Hello\tWorld");
+print("Hello\\World");
+print("My name is \"Vanshika\"");
+a = 20;
+b= 3;
+print(a/b);
+#floor division
+print(a//b);
+"""
+#practice questions- 
+num1 = int(input("Enter first number: "));
+num2 = int(input("Enter the second number: "));
+print(num1+num2);
+print(num1-num2);
+print(num1*num2);
+print(num1%num2);
+#---------------------
+num = int(input("Enter a number: "));
+if(num%2==0):
+    print("Even");
+else:
+    print("Odd");
