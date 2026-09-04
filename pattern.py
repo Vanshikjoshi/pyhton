c4f40d7c6c3ba4eb8f6e1b24567f950de0681498
Vanshika <vanshikajoshi072@gmail.com>
2026-09-04 18:59:16 +0530

    did some pattern questions

---
+++ pattern.py
@@ -0,0 +1,86 @@
+"""
+--------------------
+for i in range(1,6):
+    for j in range(1, 6):
+        print(j, end=" ");
+    print();
+--------------------------
+"""
+"""
+--------------------------------------
+for i in range(1,6):
+    for j in range(1,6):
+        print(i, end=" ");
+    print();
+--------------------
+"""
+
+"""
+------------------
+for i in range(1,6):
+    for j in range(1,i+1):
+        print("*", end=" ");
+    print();
+--------------------
+"""
+"""
+-------------------------
+for i in range(1,6):
+    for j in range(i, 0, -1):
+        print(j, end =" ");
+    print();
+-------------------------
+""" 
+
+"""
+---------------------------
+n=6;
+for i in range (1,n):
+    for j in range(1, n-i+1):
+        print(j, end=" ");
+    print();
+--------------------------
+"""
+"""
+---------------------------
+n=6;
+for i in range(1, n):
+    for j in range(n-1, i-1, -1):
+        print(j, end=" ");
+    print();
+-----------------------------------
+"""
+
+"""
+-------------------------------------
+n=6;
+for i in range(1,n):
+    for j in range(n-i, 0, -1):
+        print(j, end=" ");
+    print();
+-----------------------------
+"""
+"""
+-----------------------------
+for i in range(1,6):
+    for j in range(1, i+1):
+        print(j, end=" ");
+    print();
+for i in range(4 , 0, -1):
+    for j in range(1, i+1):
+        print(j, end=" ");
+    print();
+------------------------------
+"""
+"""
+-------------------------------------
+for i in range(5, 0, -1):
+    for i in range(5, i-1, -1):
+        print(i, end=" ");
+    print();
+for i in range(2, 6):
+    for i in range(5, i-1, -1):
+        print(i, end=" ");
+    print();
+--------------------------------------
+"""
--- session4.py
+++ session4.py
@@ -32,7 +32,36 @@ while(n<=10):
     n+=1;
 """
 #for loop
+"""
 for i in range(1,11,2):
     print(i);
 for n in range(10,0,-1):
     print(n, end=" ");
+
+sum = 0;
+for i in range(1,11):
+    sum+=i;
+print(f"\nThe sum is : {sum}");
+#concept of break
+for i in range(1,12):
+    if(i==11):
+        break;
+    if(i%2==0):
+        continue;
+    print(i);
+"""
+#practice questions------------------
+"""
+sum = 0;
+while(True):
+    num = int(input("Enter a number: "));
+    if(num<0):
+        continue;
+    elif(num>0):
+        sum+=num;
+    else:
+        break;
+print(f"The sum of all positive numbers: {sum}");
+"""
+
+    
