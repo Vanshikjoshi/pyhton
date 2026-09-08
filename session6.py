e370863b124234adca79f9a5899e9b5c19591ea4
Vanshika <vanshikajoshi072@gmail.com>
2026-09-08 18:09:32 +0530

    did some lists questions

--- session6.py
+++ session6.py
@@ -229,3 +229,36 @@ for i in range(0, rows):
 =----------------------------------------
 """
 # practice questions
+"""
+---------------------------------------
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rows = len(mat)
+cols = len(mat[0])
+for i in range(0, rows):
+    for j in range(0, cols):
+        if i != j:
+            mat[i][j] = "*"
+        print(mat[i][j], end=" ")
+    print()
+---------------------------------------------
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rows = len(mat)
+cols = len(mat[0])
+for i in range(0, rows):
+    for j in range(0, cols):
+        if i < j:
+            mat[i][j] = "*"
+        print(mat[i][j], end=" ")
+    print()
+---------------------------------------------
+"""
+mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rows = len(mat)
+cols = len(mat[0])
+for i in range(0, rows):
+    for j in range(0, cols):
+        if i + j == rows - 1:
+            print(mat[i][j], end=" ")
+        else:
+            print("*", end=" ")
+    print()
