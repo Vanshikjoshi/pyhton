fff1d2b13132bd1f566dba59940e2b6855d9cefd
Vanshika <vanshikajoshi072@gmail.com>
2026-09-20 17:32:27 +0530

    @prperty

--- Advanced-python/encapsulation.py
+++ Advanced-python/encapsulation.py
@@ -1,4 +1,5 @@
 # encapsulation - hiding data
+"""
 class Bank:
     def __init__(self, name: str, balance: int, bank_name: str) -> None:
         self.name = name  # public
@@ -20,8 +21,10 @@ c1.deposit(-2)
 # in python it is not real security because we can still access the private members like-
 print(c1._Bank__balance)
 # even we have made the balance private but still we can access this ;
+------------------------------------------------
+"""
 
-
+"""
 # getter and Setter
 class Student:
     def __inti__(self, name) -> None:
@@ -35,8 +38,31 @@ class Student:
     def set_name(self, new_name: str):
         self._name = new_name
 
-
 s1 = Student("Vanshika")
 print(s1.get_name())
 (s1.set_name("Radhika"))
 print(s1.get_name())
+"""
+
+
+# @property decorator-getter & setter new way
+class Person:
+    def __init__(self, age: int):
+        self._age = age
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        if value < 0:
+            raise ValueError("Age can't be negative")
+        self._age = value
+
+
+p = Person(22)
+print(p.age)
+p.age = 25
+print(p.age)
+p.age = -5
